731d76216bf53de87072ce063a8768735f82097f
szebestik@gmail.com <szebestik@gmail.com>
2020-12-20 21:14:19 +0100

    Added safety conditions to prevent segfault

--- dynamic array/main.cpp
+++ dynamic array/main.cpp
@@ -10,6 +10,9 @@ private:
 
 	void realloc(size_t _newCapacity)
 	{
+		if (_newCapacity == 1)
+			return;
+
 		T* temporary = new T[_newCapacity];
 		
 		memmove(temporary, buffer, currSize * sizeof(T));
@@ -43,15 +46,22 @@ public:
 
 	void erase(T* _pointer)
 	{
+		if ((_pointer - buffer) / sizeof(T) - currSize < 0)
+			return;
+
 		if (_pointer + 1 < end())
 			memmove(_pointer, _pointer + 1, currSize - (_pointer - buffer) / sizeof(T) + 1);
 
 		currSize--;
+
+		if (currSize <= capacity / 2)
+			realloc(capacity / 2);
 	}
 
 	void pop_back()
 	{
-		erase(begin() + currSize - 1);
+		if (currSize > 0)
+			erase(begin() + currSize - 1);
 	}
 
 	T* begin() const noexcept
@@ -84,9 +94,18 @@ int main()
 	dArray.push_back(3);
 	dArray.push_back(5);
 
-	dArray.erase(dArray.begin()+4);
+	dArray.erase(dArray.begin() + 4);
 
 	dArray.pop_back();
+	dArray.pop_back();
+	dArray.pop_back();
+	dArray.pop_back();
+
+	dArray.push_back(1);
+	dArray.push_back(2);
+	dArray.push_back(4);
+	dArray.push_back(3);
+	dArray.push_back(5);
 
 	for (size_t i = 0; i < dArray.size(); i++)
 		std::cout << dArray[i] << '\n';
